a109346e404e82de23ee7695ca5e441a47d85d45
Roman Frołow <rofrol@gmail.com>
2024-02-13 09:14:53 +0100

    works

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # zig-curl-tryout
+
+`zig fetch --save=curl https://github.com/jiacai2050/zig-curl/archive/424b8fdd2be9237391c5e4245fae19817ee94b36.tar.gz`
+
+`zig build run`
+
+https://github.com/jiacai2050/zig-curl
---
+++ src/main.zig
@@ -0,0 +1,86 @@
+const std = @import("std");
+const println = @import("util.zig").println;
+const mem = std.mem;
+const Allocator = mem.Allocator;
+const curl = @import("curl");
+const Easy = curl.Easy;
+
+fn get(allocator: Allocator, easy: Easy) !void {
+    const resp = try easy.get("https://httpbin.org/anything");
+    defer resp.deinit();
+
+    std.debug.print("Status code: {d}\nBody: {s}\n", .{
+        resp.status_code,
+        resp.body.items,
+    });
+
+    const Response = struct {
+        headers: struct {
+            Host: []const u8,
+        },
+        method: []const u8,
+    };
+    const parsed = try std.json.parseFromSlice(Response, allocator, resp.body.items, .{
+        .ignore_unknown_fields = true,
+    });
+    defer parsed.deinit();
+
+    try std.testing.expectEqualDeep(parsed.value, Response{
+        .headers = .{ .Host = "httpbin.org" },
+        .method = "GET",
+    });
+}
+
+fn post(allocator: Allocator, easy: Easy) !void {
+    const payload =
+        \\{"name": "John", "age": 15}
+    ;
+    const resp = try easy.post("https://httpbin.org/anything", "application/json", payload);
+    defer resp.deinit();
+
+    // std.debug.print("Status code: {d}\nBody: {s}\n", .{
+    //     resp.status_code,
+    //     resp.body.items,
+    // });
+
+    const Response = struct {
+        headers: struct {
+            @"Content-Type": []const u8,
+        },
+        json: struct {
+            name: []const u8,
+            age: u32,
+        },
+        method: []const u8,
+    };
+    const parsed = try std.json.parseFromSlice(Response, allocator, resp.body.items, .{ .ignore_unknown_fields = true });
+    defer parsed.deinit();
+
+    // sometimes I need to use stdout separately, otherwise stringify does not print what it should.
+    // it prints beginning of parsed.value, instead of parsed.value.json.
+    // const stdout = std.io.getStdOut().writer();
+    // try stdout.writeByte('\n');
+
+    try std.json.stringify(&parsed.value.json, .{}, std.io.getStdOut().writer());
+
+    try std.testing.expectEqualDeep(parsed.value, Response{
+        .headers = .{ .@"Content-Type" = "application/json" },
+        .json = .{ .name = "John", .age = 15 },
+        .method = "POST",
+    });
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit() != .ok) @panic("leak");
+    const allocator = gpa.allocator();
+
+    const easy = try Easy.init(allocator, .{});
+    defer easy.deinit();
+
+    // println("GET demo");
+    // try get(allocator, easy);
+
+    // println("POST demo");
+    try post(allocator, easy);
+}
